9f569aa998d732a45576f7da378743cc93fa64c2
StorytellerCZ <storyteller@freedombase.net>
2018-03-03 08:18:25 +0100

    Formatting and other fixes

--- community-services/wattpad.js
+++ community-services/wattpad.js
@@ -1,19 +1,18 @@
-
 if (Meteor.isClient) {
-  Meteor.linkWithWattpad = function (options, callback) {
+  Meteor.linkWithWattpad = function(options, callback) {
     if (!Meteor.userId()) {
       throw new Meteor.Error(402, 'Please login to an existing account before link.');
     }
-    if(!Package['storyteller:accounts-wattpad']) {
-      throw new Meteor.Error(403, 'Please include storyteller:accounts-wattpad package.')
+    if (!Package['storyteller:accounts-wattpad']) {
+      throw new Meteor.Error(403, 'Please include storyteller:accounts-wattpad package.');
     }
 
-    if (!callback && typeof options === "function") {
+    if (!callback && typeof options === 'function') {
       callback = options;
       options = null;
     }
 
     var credentialRequestCompleteCallback = Accounts.oauth.linkCredentialRequestCompleteHandler(callback);
-    Package['storyteller:accounts-wattpad'].Wattpad.requestCredential(options, credentialRequestCompleteCallback);
+    Wattpad.requestCredential(options, credentialRequestCompleteCallback);
   };
 }
--- package.js
+++ package.js
@@ -2,14 +2,14 @@ var fs = Npm.require('fs');
 var path = Npm.require('path');
 
 Package.describe({
-  "summary": "Meteor external service link system",
-  "version": "1.2.11",
-  "git": "https://github.com/yubozhao/meteor-link-accounts",
-  "name": "bozhao:link-accounts",
-  "description": "Link social accounts for Meteor"
+  summary: 'Meteor external service link system',
+  version: '1.2.12',
+  git: 'https://github.com/yubozhao/meteor-link-accounts',
+  name: 'bozhao:link-accounts',
+  description: 'Link social accounts for Meteor'
 });
 
-Package.on_use(function (api) {
+Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
 
   api.imply('accounts-base', ['client', 'server']);
@@ -18,42 +18,45 @@ Package.on_use(function (api) {
 
   api.add_files('link_accounts_client.js', 'client');
   api.add_files('link_accounts_server.js', 'server');
-  api.add_files([
-    'core-services/meteor_developer.js',
-    'core-services/facebook.js',
-    'core-services/github.js',
-    'core-services/google.js',
-    'core-services/meetup.js',
-    'core-services/twitter.js',
-    'core-services/weibo.js',
-    'community-services/angellist.js',
-    'community-services/dropbox.js',
-    'community-services/discord.js',
-    'community-services/edmodo.js',
-    'community-services/instagram.js',
-    'community-services/linkedin.js',
-    'community-services/mailru.js',
-    'community-services/qq.js',
-    'community-services/ok.js',
-    'community-services/slack.js',
-    'community-services/spotify.js',
-    'community-services/soundcloud.js',
-    'community-services/twitch.js',
-    'community-services/venmo.js',
-    'community-services/vk.js',
-    'community-services/wechat.js',
-    'community-services/wattpad.js'
-  ], 'client');
+  api.add_files(
+    [
+      'core-services/meteor_developer.js',
+      'core-services/facebook.js',
+      'core-services/github.js',
+      'core-services/google.js',
+      'core-services/meetup.js',
+      'core-services/twitter.js',
+      'core-services/weibo.js',
+      'community-services/angellist.js',
+      'community-services/dropbox.js',
+      'community-services/discord.js',
+      'community-services/edmodo.js',
+      'community-services/instagram.js',
+      'community-services/linkedin.js',
+      'community-services/mailru.js',
+      'community-services/qq.js',
+      'community-services/ok.js',
+      'community-services/slack.js',
+      'community-services/spotify.js',
+      'community-services/soundcloud.js',
+      'community-services/twitch.js',
+      'community-services/venmo.js',
+      'community-services/vk.js',
+      'community-services/wechat.js',
+      'community-services/wattpad.js'
+    ],
+    'client'
+  );
 });
 
-function isPackageAvailable (packageName) {
-  function isAppDir (dir) {
+function isPackageAvailable(packageName) {
+  function isAppDir(dir) {
     try {
       return fs.statSync(path.join(filepath, '.meteor', 'packages')).isFile();
     } catch (e) {
       return false;
     }
-  };
+  }
 
   var currentDir = process.cwd();
   while (currentDir) {
@@ -66,9 +69,9 @@ function isPackageAvailable (packageName) {
     } else {
       currentDir = newDir;
     }
-  };
+  }
 
   var meteorPackages = fs.readFileSync(path.join(currentDir, '.meteor', 'packages'), 'utf8');
   var packageName = new RegExp(packageName);
   return !!meteorPackages.match(packageName);
-};
+}
